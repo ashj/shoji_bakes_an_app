e788520a1242daabe53998f66cc20de551bb4493
lsitec333.asato <andre.shoji@lsitec.org.br>
2017-11-28 17:43:10 -0200

    [espresso] test first known recipe ingredient
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/androidTest/java/com/example/shoji/bakingapp/MainActivityBasicTest.java
+++ app/src/androidTest/java/com/example/shoji/bakingapp/MainActivityBasicTest.java
@@ -1,14 +1,21 @@
 package com.example.shoji.bakingapp;
 
+import android.content.ClipData;
+import android.support.annotation.NonNull;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.example.shoji.bakingapp.ui.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -19,6 +26,9 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.core.internal.deps.dagger.internal.Preconditions.checkNotNull;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -44,7 +54,7 @@ public class MainActivityBasicTest {
 
 
     @Test
-    public void idlingResourceTest() {
+    public void testIdlingResourceAndOneIngredient() {
         String text = "2 CUP Graham Cracker crumbs";
         int recipePosition = 0;
         int ingredientPosition = 1;
@@ -61,6 +71,11 @@ public class MainActivityBasicTest {
                 .perform(RecyclerViewActions.actionOnItemAtPosition(ingredientListPosition, click()));
                //check(matches(withText(text))));
 
+        onView(withId(R.id.activity_recipe_ingredients))
+                .perform(scrollToPosition(ingredientListPosition))
+                .check(matches(atPosition(ingredientListPosition, withText(text))));
+
+
 
     }
 
@@ -72,4 +87,25 @@ public class MainActivityBasicTest {
         }
     }
 
+    //https://stackoverflow.com/questions/31394569/how-to-assert-inside-a-recyclerview-in-espresso
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
 }
