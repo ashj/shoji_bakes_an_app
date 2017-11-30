41b7f4938b06f9694ba9f7229e11e9ad5eba7383
lsitec333.asato <andre.shoji@lsitec.org.br>
2017-11-30 13:00:55 -0200

    [widget][wip] add ingredient list widget
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

---
+++ app/src/main/java/com/example/shoji/bakingapp/widget/WidgetIngredientListConfigurationActivity.java
@@ -0,0 +1,15 @@
+package com.example.shoji.bakingapp.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shoji.bakingapp.R;
+
+public class WidgetIngredientListConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_ingredient_list_configuration);
+    }
+}
